c305a706bd4dd1d588cef0ea4f20f7b52857f804
John Baldo <1191015+jbaldo@users.noreply.github.com>
2019-11-24 12:24:52 -0800

    put in an express router

---
+++ routes/index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const Router = require('express').Router;
+const Request = require('express').Request;
+
+const pg = require('pg');
+
+
+const connectionName =
+  process.env.INSTANCE_CONNECTION_NAME || 'openousd:us-central1:openousd-staging';
+const dbUser = process.env.SQL_USER;
+const dbPassword = process.env.SQL_PASSWORD;
+const dbName = process.env.SQL_NAME;
+
+const pgConfig = {
+  max: 1,
+  user: dbUser,
+  password: dbPassword,
+  database: dbName,
+  host: `/cloudsql/${connectionName}`
+}
+
+
+// if (process.env.NODE_ENV === 'production') {
+//   pgConfig.host = `/cloudsql/${connectionName}`;
+//}
+
+// Connection pools reuse connections between invocations,
+// and handle dropped or expired connections automatically.
+let pgPool;
+
+const router = Router()
+
+router.get('/', async (req, res, next) => {
+    try {
+        // Initialize the pool lazily, in case SQL access isn't needed for this
+        // GCF instance. Doing so minimizes the number of active SQL connections,
+        // which helps keep your GCF instances under SQL connection limits.
+        if (!pgPool) {
+            pgPool = new pg.Pool(pgConfig);
+        }
+
+        pgPool.query('SELECT * FROM objects LIMIT 10', (err, results) => {
+            if (err) {
+              console.error(err);
+              res.status(500).send(err);
+            } else {
+              res.json(results);
+            }
+        });
+
+        // Close any SQL resources that were declared inside this function.
+        // Keep any declared in global scope (e.g. mysqlPool) for later reuse.
+
+    } catch(e) {
+        next(e);
+    }
+});
+
+module.exports = router;
