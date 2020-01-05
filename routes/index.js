b19db0d19bfef1a15da7bdcdbbdc934ae98acb95
John Baldo <1191015+jbaldo@users.noreply.github.com>
2020-01-05 13:39:06 -0800

    remove semicolons in routes, add support for grouping by restricted

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const Router = require('express').Router;
-const Request = require('express').Request;
+const express = require('express')
+const Router = require('express').Router
+const Request = require('express').Request
 
-const pg = require('pg');
+const pg = require('pg')
 
 
 const connectionName =
-  process.env.INSTANCE_CONNECTION_NAME || 'openousd:us-central1:openousd-staging';
-const dbUser = process.env.SQL_USER;
-const dbPassword = process.env.SQL_PASSWORD;
-const dbName = process.env.SQL_NAME;
+  process.env.INSTANCE_CONNECTION_NAME || 'openousd:us-central1:openousd-staging'
+const dbUser = process.env.SQL_USER
+const dbPassword = process.env.SQL_PASSWORD
+const dbName = process.env.SQL_NAME
 
 const pgConfig = {
   max: 1,
@@ -23,29 +23,29 @@ const pgConfig = {
 // should be conversted to floats in js
 // https://github.com/brianc/node-pg-types/issues/28
 var types = require('pg').types
-types.setTypeParser(1700, 'text', parseFloat);
+types.setTypeParser(1700, 'text', parseFloat)
 
 
 // if (process.env.NODE_ENV === 'production') {
-//   pgConfig.host = `/cloudsql/${connectionName}`;
+//   pgConfig.host = `/cloudsql/${connectionName}`
 //}
 
 // Connection pools reuse connections between invocations,
 // and handle dropped or expired connections automatically.
-let pgPool;
+let pgPool
 
 if (!pgPool) {
-        pgPool = new pg.Pool(pgConfig);
+        pgPool = new pg.Pool(pgConfig)
 }
 
 const router = Router()
 
 router.get('/departments', async (req, res, next) => {
 
-    var year = 2018;
+    var year = 2018
 
     if("year" in req.query) {
-        year = req.query.year;
+        year = req.query.year
     }
 
     var query = `SELECT e.site_code as code, s.description as name,
@@ -56,29 +56,34 @@ router.get('/departments', async (req, res, next) => {
                     LEFT JOIN sites s ON e.site_code = s.code
                     WHERE e.site_code >= 900
                     AND e.year = ${year}
-                    GROUP BY e.site_code, s.description, e.year`;
+                    GROUP BY e.site_code, s.description, e.year`
 
-    let processor;
+    let processor
 
     try {
-        getData(query, res, processor);
+        getData(query, res, processor)
     } catch(e) {
-        res.status(500).send(err);
+        res.status(500).send(err)
     }
 
-});
+})
 
 router.get('/sankey', async (req, res, next) => {
 
-    var year = 2018;
-    var minSpend = 100000;
+    var year = 2018
+    var minSpend = 100000
+    var groupBy = null
 
     if("year" in req.query) {
-        year = req.query.year;
+        year = req.query.year
     }
 
     if("minSpend" in req.query) {
-        minSpend = req.query.minSpend;
+        minSpend = req.query.minSpend
+    }
+
+    if("groupBy" in req.query) {
+        groupBy = req.query.groupBy
     }
 
     var nodesQuery = `SELECT r.category as id, 'resource' as type --, e.resource_code as code, r.description
@@ -99,34 +104,64 @@ router.get('/sankey', async (req, res, next) => {
                       WHERE e.year = ${year}
                       AND e.site_code >= 900
                       GROUP BY s.category
-                      HAVING SUM(e.ytd_actual) >= ${minSpend}`;
+                      HAVING SUM(e.ytd_actual) >= ${minSpend}`
 
-    var linksQuery = `SELECT SUM(e.ytd_actual) as value, s.category as target, r.category as source
+    var resourceCol = "category"
+    if (groupBy === "restricted") resourceCol = "type"
+
+    var linksQuery = `SELECT SUM(e.ytd_actual) as value, s.category as target, r.${resourceCol} as source
                       FROM expenditures e
                       LEFT JOIN sites s ON e.site_code = s.code
                       LEFT JOIN resources r ON e.resource_code = r.code
-                      WHERE e.year = 2018
+                      WHERE e.year = ${year}
                       AND e.site_code >= 900
-                      GROUP BY s.category, r.category
+                      GROUP BY s.category, r.${resourceCol}
                       HAVING SUM(e.ytd_actual) >= ${minSpend}`
 
+    var linksResourceTypeQuery = `UNION ALL
+
+                      SELECT SUM(e.ytd_actual) as value, r.type as target, r.category as source
+                      FROM expenditures e
+                      LEFT JOIN sites s ON e.site_code = s.code
+                      LEFT JOIN resources r ON e.resource_code = r.code
+                      WHERE e.year = ${year}
+                      AND e.site_code >= 900
+                      GROUP BY r.category, r.type
+                      HAVING SUM(e.ytd_actual) >= ${minSpend}`
+
+    if (groupBy === "restricted") linksQuery = linksQuery + linksResourceTypeQuery
+
+    const resourceTypeNodes = [
+        {
+          "id": "Restricted",
+          "type": "resource_type"
+        },
+        {
+          "id": "Unrestricted",
+          "type": "resource_type"
+        }
+    ]
+
     try {
-        const nodes = await pgPool.query(nodesQuery)
-        const links = await pgPool.query(linksQuery)
+        var nodes = await pgPool.query(nodesQuery)
+        var links = await pgPool.query(linksQuery)
+
+        nodes = nodes.rows
+        if (groupBy === "restricted") nodes = nodes.concat(resourceTypeNodes)
 
         let result = {
-          nodes: nodes.rows,
+          nodes: nodes,
           links: links.rows
         }
 
         res.json(result)
     } catch(e) {
         console.log(e.stack)
-        throw new Error (e);
-        res.status(500).send(e);
+        throw new Error (e)
+        res.status(500).send(e)
     }
 
-});
+})
 
 const getData = (query, res, processor) => {
 
@@ -135,17 +170,17 @@ const getData = (query, res, processor) => {
     // which helps keep your GCF instances under SQL connection limits.
 
     if (!pgPool) {
-        pgPool = new pg.Pool(pgConfig);
+        pgPool = new pg.Pool(pgConfig)
     }
 
     pgPool.query(query, (err, results) => {
         if (err) {
-          console.log(err);
-          throw new Error (err);
+          console.log(err)
+          throw new Error (err)
         } else {
-          res.json(results.rows);
+          res.json(results.rows)
         }
-    });
+    })
 }
 
-module.exports = router;
+module.exports = router
