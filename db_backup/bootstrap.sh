4042e609c40b8a0a58e21482d9bfb6bedac4067e
John Baldo <1191015+jbaldo@users.noreply.github.com>
2020-09-07 20:27:19 -0700

    update snapshot

--- db_backup/bootstrap.sh
+++ db_backup/bootstrap.sh
@@ -1,2 +1,2 @@
 # assumes there is already a database called openousd
-pg_restore -U postgres --clean -d openousd /db_backup/snapshots/openousd_snapshot_2020-09-06_17-49.dump
+pg_restore -U postgres --clean -d openousd /db_backup/snapshots/openousd_snapshot_2020-09-08_00-13.dump
