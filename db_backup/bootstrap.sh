2ca18564588cb0d0caf5e1176e16998d139456e3
John Baldo <1191015+jbaldo@users.noreply.github.com>
2020-11-22 16:10:51 -0800

    upload 2019-20 expenditure data

--- db_backup/bootstrap.sh
+++ db_backup/bootstrap.sh
@@ -1,2 +1,2 @@
 # assumes there is already a database called openousd
-pg_restore -U postgres --clean -d openousd /db_backup/snapshots/openousd_snapshot_2020-09-20_06-31.dump
+pg_restore -U postgres --clean -d openousd /db_backup/snapshots/openousd_snapshot_2020-11-23_00-09.dump
