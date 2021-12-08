ceb0f014eda8671a385831da37ae71e686d05352
jbaldo <1191015+jbaldo@users.noreply.github.com>
2021-12-08 09:53:47 -0800

    Update Central Program Sankey Queries (#16)
    * categorize central programs sankey objects
    
    * remove object name from links querry grouping
    
    * add restricted and unrestricted nodes, change category name to supplies
    
    * update to latest snapshot

--- db_backup/bootstrap.sh
+++ db_backup/bootstrap.sh
@@ -1,2 +1,2 @@
 # assumes there is already a database called openousd
-pg_restore -U postgres --clean -d openousd /db_backup/snapshots/openousd_snapshot_2020-11-23_00-09.dump
+pg_restore -U postgres --clean -d openousd /db_backup/snapshots/openousd_snapshot_2021-09-25_20-17-2020-21-expenditures.dump
