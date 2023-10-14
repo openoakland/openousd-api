cced6d8a7d48472525e0cae51fe52f6340700add
John Baldo <1191015+jbaldo@users.noreply.github.com>
2023-10-14 11:04:53 -0700

    Data Update for 2021-22 (#28)
    * update year and fix bug with roles match on job class id
    
    * add various snapshots for the annual data update
    
    * update bootstrap to latest db

--- db_backup/bootstrap.sh
+++ db_backup/bootstrap.sh
@@ -1,2 +1,2 @@
 # assumes there is already a database called openousd
-pg_restore -U postgres --clean -d openousd /db_backup/snapshots/openousd_snapshot_2021-09-25_20-17-2020-21-expenditures.dump
+pg_restore -U postgres --clean -d openousd /db_backup/snapshots/openousd_snapshot_2023-02-24_03-36-2021-22-update-fix-staff-roles.dump
